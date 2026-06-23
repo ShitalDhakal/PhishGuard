cd72fa8ed7207c0249afbe82b259135a6d417053
DevenStha <dhakalshital8@gmail.com>
2026-06-24 05:31:14 +0545

    ok

--- Mailbox/services/user_data.py
+++ Mailbox/services/user_data.py
@@ -3,14 +3,17 @@ from django.http import HttpResponse, JsonResponse
 
 from analyzer.models import IOC
 from Mailbox.models import EmailRecord
+from analyzer.services.email_parser import parse_email
+from analyzer.services.ioc_extractor import extract_iocs, save_iocs
 
-def fetch_ioc(request):
+async def fetch_ioc(request):
     try:
         data = {}
         if(request.body):
             data = json.loads(request.body)\
         
         ioc_list = []
+        await load_ioc(request)
         if(data.get('email')):
             ioc_list = IOC.objects.filter(email=data['email']).values()
         else:
@@ -20,9 +23,21 @@ def fetch_ioc(request):
         print(f"Error: {e}")
         return HttpResponse(500)
     
-def load_ioc(request):
+async def load_ioc(request):
     try:
-        emails = E
+        emails = list(EmailRecord.objects.filter(scanned=False).values())
+        email_with_iocs = []
+        for email in emails:
+            parsed_email = parse_email(email)
+            email_with_iocs.append({"email":parsed_email, "iocs":extract_iocs(parsed_email)})
+
+        for email in email_with_iocs:
+            email_record = email.get("email")
+            iocs = email.get("iocs")
+            save_iocs(email_record, iocs)
+
+        EmailRecord.objects.update(scanned=True)
+        return JsonResponse({"message":"IOCs loaded successfully.", "status": 200})
     except Exception as e:
         print(f"Error: {e}")
         return HttpResponse(500)
